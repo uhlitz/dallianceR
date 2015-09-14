38a13d390261b75e2c59a473db1cd9b5284d54bc
uhlitz <uhlitz@gmail.com>
2015-09-14 13:44:41 +0200

    example data modified

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 dummy <- data_frame(Experiment = rep(c("MCF7", "HEK293"), times = c(6, 4)),
-                    Sample = rep(c("A", "B"), times = 5),
+                    Sample = c("A", "B", "A", "B", "A", "B", "C", "D", "C", "D"),
                     Replicate = c(1, 1, 2, 2, 3, 3, 1, 1, 2, 2),
                     bigwig = paste0("examples/bigwig/",
                                     list.files("examples/bigwig/", pattern = ".bw$")))
