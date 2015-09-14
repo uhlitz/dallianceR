0792ac7227aba681d11439978dd8693376b8165d
frenkiboy <vedran.franke@gmail.com>
2015-09-14 13:21:16 +0200

    DESCRIPTION: 	- removed the dalliance import - was a mistake 	- added Maintainer
    NAMESPACE:
    	- removed dalliance import
    
    dalliance.R:
    	- removed library functions, dummy data and function call
    
    data.R:
    	- creates the data annotations
    
    gbdr.R:
    	- added the parentheses to the function

--- R/dalliance.R
+++ R/dalliance.R
@@ -1,18 +1,3 @@
-# devtools::install_github("hadley/readr")
-# devtools::install_github("hadley/dplyr")
-# devtools::install_github("hadley/magrittr")
-# install.packages("RColorBrewer")
-library(readr)
-library(dplyr)
-library(magrittr)
-library(RColorBrewer)
-
-dummy <- data_frame(Experiment = rep(c("MCF7", "HEK293"), times = c(6, 4)),
-                    Sample = rep(c("A", "B"), times = 5),
-                    Replicate = c(1, 1, 2, 2, 3, 3, 1, 1, 2, 2),
-                    bigwig = paste0("examples/bigwig/",
-                                   list.files("examples/bigwig/", pattern = ".bw$")))
-
 dalliance <- function(input, color = "Dark2") {
 
   input %>%
@@ -84,5 +69,4 @@ dalliance <- function(input, color = "Dark2") {
     }
 }
 
-dalliance(dummy)
 
