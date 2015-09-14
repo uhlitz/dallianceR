b7cc5a4620f76d16b116598c59c1b0fb57b27aff
uhlitz <uhlitz@gmail.com>
2015-09-14 11:47:06 +0200

    removed large example files >100mb

--- R/dalliance.R
+++ R/dalliance.R
@@ -3,9 +3,9 @@ library(dplyr)
 library(magrittr)
 library(RColorBrewer)
 
-dummy <- data_frame(Experiment = rep(c("MCF7", "HEK293"), times = c(8, 4)),
-                    Sample = rep(c("A", "B"), times = 6),
-                    Replicate = c(1, 1, 2, 2, 3, 3, 4, 4, 1, 1, 2, 2),
+dummy <- data_frame(Experiment = rep(c("MCF7", "HEK293"), times = c(6, 4)),
+                    Sample = rep(c("A", "B"), times = 5),
+                    Replicate = c(1, 1, 2, 2, 3, 3, 1, 1, 2, 2),
                     bigwig = paste0("examples/bigwig/",
                                    list.files("examples/bigwig/", pattern = ".bw$")))
 
