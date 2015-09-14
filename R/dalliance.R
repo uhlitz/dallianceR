0116a9cc08dbedeced4822d85c9dabcec06d966a
uhlitz <uhlitz@gmail.com>
2015-09-14 13:22:12 +0200

    updated gbdr_sketch.Rmd to work with example data

--- R/dalliance.R
+++ R/dalliance.R
@@ -7,11 +7,12 @@ library(dplyr)
 library(magrittr)
 library(RColorBrewer)
 
-input <- data_frame(Experiment = rep(c("MCF7", "HEK293"), times = c(6, 4)),
-                    Sample = c("A", "B", "A", "B", "A", "B", "C", "D", "C", "D"),
-                    Replicate = c(1, 1, 2, 2, 3, 3, 1, 1, 2, 2),
-                    bigwig = paste0("examples/bigwig/",
-                                   list.files("examples/bigwig/", pattern = ".bw$")))
+example <- data_frame(Experiment = rep(c("MCF7", "HEK293"), times = c(6, 4)),
+                      Sample = c("A", "B", "A", "B", "A", "B", "C", "D", "C", "D"),
+                      Replicate = c(1, 1, 2, 2, 3, 3, 1, 1, 2, 2),
+                      bigwig = paste0(
+                        "examples/bigwig/",
+                        list.files("examples/bigwig/", pattern = ".bw$")))
 
 dalliance <- function(input = data_frame(Experiment = 1,
                                          Sample = 1,
